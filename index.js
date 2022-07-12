5646c45c1572a3a5c2cce8158db9d1a2f7f82783
Bright Mensah <mensahokobright@gmail.com>
2022-07-12 19:35:10 +0000

    Done with addition

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 let form = document.getElementById("forms");
 
 let btn = (document.querySelector(".btn").onclick = () => {
@@ -28,4 +26,58 @@ function wait() {
   }, 4000);
 }
 
+// get the addition form
+let additionForm = document.getElementById("addition-form");
 
+let addBtn = (document.querySelector(".addBtn").onclick = (e) => {
+  e.preventDefault();
+  //  get first number and second number
+  let firstNumber = parseInt(document.getElementById("fNumber").value);
+  let secondNumber = parseInt(document.getElementById("sNumber").value);
+  let getLoader = document.querySelector(".addAnim");
+  let getBtn = document.querySelector(".addBtn");
+  let displayResult = document.querySelector(".addition-result");
+  let showResult = document.querySelector("#result");
+
+  // check if first number and last number are not empty
+  if (
+    document.getElementById("fNumber").value === "" &&
+    document.getElementById("sNumber").value === ""
+  ) {
+    //  don't do anything since the required validation is already there
+  } else if (document.getElementById("sNumber").value === "") {
+    // alert('sdsd')
+  } else {
+    additionForm.onclick = (e) => {
+      getBtn.classList.add("hideBtn");
+      getLoader.classList.add("showAddAnim");
+      e.preventDefault();
+
+      getResult();
+      setTimeout(() => {
+        function reload() {
+          location.reload();
+        }
+        reload();
+      }, 12000);
+    };
+
+    let getResult = () => {
+      setTimeout(() => {
+        getLoader.classList.remove("showAddAnim");
+        let result = firstNumber + secondNumber;
+        displayResult.classList.add("showResult");
+        displayResult.innerHTML = `The result for adding  ${firstNumber} + ${secondNumber}  is  ${(showResult.innerHTML =
+          result)} `;
+        setTimeout(() => {
+          getBtn.classList.remove("hideBtn");
+          displayResult.classList.remove("showResult");
+          document.getElementById("fNumber").value = "";
+          document.getElementById("sNumber").value = "";
+        }, 5000);
+      }, 6000);
+    };
+  }
+});
+
+//  addition ends here
